5fbf9554f558bc6e783cc5c74e989c126ce7235e
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-30 19:26:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,13 @@
 katz_deli = []
 
 def line(arr)
+  new_arr = []
   if arr.length == 0
     puts "there is nobody in line"
     else
+    for name in array
+      badges << badge_maker(name)
+    end
+    return badges
     end
 end
