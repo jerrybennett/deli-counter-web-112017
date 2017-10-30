d1be015bc4d179281bb5098de40854fc208eff34
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-30 20:41:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,8 @@ def line(arr)
   if arr.length == 0
     puts "there is nobody in line"
     else
-      new_arr.push(arr.each.with_index {
-        |val, index|
-        })
-    return badges
+    arr.each.with_index { |val, index|
+      puts ""
+        }
     end
 end
