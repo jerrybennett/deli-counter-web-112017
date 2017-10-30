4e71750c4bf618ce4659c7766f722172c3f23402
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-30 21:13:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,8 +19,8 @@ def take_a_number(katz_deli, str)
 end
 
 def now_serving(katz_deli)
-  if katz_deli == 0
-    puts "The line is currently empty."
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.shift}."
   end
