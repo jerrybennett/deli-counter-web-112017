aa177a6fbf801a4f4548c007825786103b86b3b1
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-30 20:43:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length == 0
-    puts "there is nobody in line"
+    puts "The line is currently empty."
     else
     katz_deli.each.with_index { |val, index|
       puts "The line is currently: #{index}. #{val}"
