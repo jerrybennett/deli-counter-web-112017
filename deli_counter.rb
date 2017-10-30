cf89270e6c2a68a0c95245c9f0085a15c1dfa28b
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-30 20:42:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,11 @@
 # Write your code here.
 katz_deli = []
 
-def line(arr)
-  new_arr = []
-  if arr.length == 0
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "there is nobody in line"
     else
-    arr.each.with_index { |val, index|
+    katz_deli.each.with_index { |val, index|
       puts "The line is currently: #{index}. #{val}"
         }
     end
